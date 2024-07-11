6d79dc94322491ee94bcba57e0cf09ace251d70e
SayantanDas10 <154346035+SayantanDas10@users.noreply.github.com>
2024-07-11 20:46:28 +0000

    finally done

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -15,3 +15,16 @@ let y =0;
 console.log(typeof(y));
 let r = String(y);
 console.log(typeof(r));
+//
+let s1 = "HELLO"
+let s2 = "Sayantan"
+let s3 = s1+" "+s2;
+console.log(s3)
+//
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
