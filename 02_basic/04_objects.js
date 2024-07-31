a5c94c55c962a8120da7bc16366f2bc3760414f8
SayantanDas10 <154346035+SayantanDas10@users.noreply.github.com>
2024-07-31 21:09:21 +0000

    hello

---
+++ 02_basic/04_objects.js
@@ -0,0 +1,23 @@
+//const td = new Object();  ->Singleton-a java singleton class is a class that can have only one object (an instance of the class) at a time.
+const td = {};
+td.id = "123";
+td.name = "Sam";
+td.isLoggedIn = false;
+console.log(td);
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "hitesh",
+            lastname: "choudhary"
+        }
+    }
+}
+console.log(regularUser.fullname.userfullname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+//const obj3 = { obj1, obj2 }
+//const obj3 = Object.assign({},obj1, obj2);
+const obj3 = {...obj1, ...obj2}
+console.log(obj3);
